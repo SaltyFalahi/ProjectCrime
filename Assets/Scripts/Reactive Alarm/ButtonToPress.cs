a4157c07d6f1be08b8d90fea6be056cf9b18c789
Madshtik <jann_cosio@yahoo.com>
2020-04-23 19:36:35 +0400

    Added 2/3 Prototype (incomplete)

---
+++ Assets/Scripts/Reactive Alarm/ButtonToPress.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class ButtonToPress : MonoBehaviour
+{
+    public TextMeshProUGUI ButtonText;
+
+    string[] buttons = new string[4] { "A", "B", "X", "Y" };
+
+    string readyStr = "Ready?";
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ButtonText.text = readyStr.ToString();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
