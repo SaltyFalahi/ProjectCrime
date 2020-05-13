5c781f57ef3b3ccf29b0db5a05559817f9daf438
SaltyFalahi <suliflin@gmail.com>
2020-05-13 04:51:33 +0400

    Added Hot Potato Minigame

---
+++ Assets/Scripts/Hot Potato/HotPotato.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HotPotato : MonoBehaviour
+{
+    public List<GameObject> players;
+
+    public GameObject bomb;
+    public GameObject player;
+
+    public float timer;
+
+    float countdown;
+
+    void Start()
+    {
+        countdown = timer;
+        SpawnBomb();
+    }
+
+    void Update()
+    {
+        countdown -= Time.deltaTime;
+
+        if (countdown <= 0)
+        {
+            players.Remove(player);
+            Destroy(player);
+            SpawnBomb();
+        }
+
+        bomb.transform.position = new Vector3(player.transform.position.x, 5, player.transform.position.z);
+    }
+
+    void SpawnBomb()
+    {
+        if (players.Count > 1)
+        {
+            player = players[Random.Range(0, players.Count)];
+            countdown = timer;
+        }
+        else
+        {
+            player = players[0];
+            Debug.Log(player.name + " Won");
+        }
+    }
+
+    public void PassBomb(GameObject obj)
+    {
+        player = obj;
+    }
+}
---
+++ Assets/Scripts/Hot Potato/HotPotatoMovement.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HotPotatoMovement : MonoBehaviour
+{
+    public enum player
+    {
+        P1,
+        P2,
+        P3,
+        P4
+    }
+
+    public player type;
+
+    public HotPotato instance;
+
+    public int moveSpeed;
+
+    Rigidbody rb;
+
+    string movementH;
+    string movementV;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        GetPlayer();
+    }
+
+    void Update()
+    {
+        Vector3 movement = new Vector3(Input.GetAxis(movementH), 0, Input.GetAxis(movementV));
+
+        rb.MovePosition(transform.position + movement * moveSpeed * Time.deltaTime);
+    }
+
+    void GetPlayer()
+    {
+        switch (type)
+        {
+            case player.P1:
+                movementH = "P1Horizontal";
+                movementV = "P1Vertical";
+                break;
+
+            case player.P2:
+                movementH = "P2Horizontal";
+                movementV = "P2Vertical";
+                break;
+
+            case player.P3:
+                movementH = "P3Horizontal";
+                movementV = "P3Vertical";
+                break;
+
+            case player.P4:
+                movementH = "P4Horizontal";
+                movementV = "P4Vertical";
+                break;
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (instance.player == gameObject && other.CompareTag("Player"))
+        {
+            Debug.Log("hi");
+            instance.PassBomb(other.gameObject);
+        }
+    }
+}
