212aaf7ae96ebd3a95283bbcd2607a6bf12868a2
Madshtik <jann_cosio@yahoo.com>
2020-05-30 17:24:55 +0400

    -Added SethScene -Added BoardUIManager -Added an incomplete PathFollowing script - Use this to make the pathfollowing algorithm -Added DiceRoller script - Use this for pathfollowing; message me for any questions on set up

---
+++ Assets/Scripts/BoardUIManager.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BoardUIManager : MonoBehaviour
+{
+    public Camera boardCam;
+
+    public GameObject boardPlayerCamPoint;
+    public GameObject boardMapCamPoint;
+    public GameObject mainPanel;
+    public GameObject dicePanel;
+    public GameObject itemsPanel;
+
+    public void OpenDicePanel()
+    {
+        if (!dicePanel.activeInHierarchy)
+        {
+            mainPanel.SetActive(false);
+            dicePanel.SetActive(true);
+            itemsPanel.SetActive(false);
+        }
+    }
+
+    public void OpenItemsPanel()
+    {
+        if (!itemsPanel.activeInHierarchy)
+        {
+            mainPanel.SetActive(false);
+            dicePanel.SetActive(false);
+            itemsPanel.SetActive(true);
+        }
+    }
+
+    public void OpenMainPanel()
+    {
+        if (!mainPanel.activeInHierarchy)
+        {
+            mainPanel.SetActive(true);
+            dicePanel.SetActive(false);
+            itemsPanel.SetActive(false);
+        }
+    }
+
+    public void OpenBoardMapMode()
+    {
+        
+    }
+
+    public void ReturnToPlayerCamMode()
+    {
+
+    }
+}
---
+++ Assets/Scripts/PathFollowing.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PathFollowing : MonoBehaviour
+{
+    public List<Transform> tilePoints = new List<Transform>();
+
+    public GameObject player;
+
+    DiceRoller diceRoller;
+
+    public float speed = 2.5f;
+    public float reachDist = 1;
+
+    int index = 0;
+    int spacesToMove;
+    int currentTileNum;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        diceRoller = GameObject.FindGameObjectWithTag("DiceObj").GetComponent<DiceRoller>();
+        spacesToMove = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (diceRoller.diceRolled)
+        {
+            MovePlayer();
+        }
+    }
+
+    void MovePlayer()
+    {
+        
+    }
+}
