aa37ed544c18b86bc8246d299e3f00540de307d2
Madshtik <jann_cosio@yahoo.com>
2020-05-31 19:59:36 +0400

    -Added 2 types of map view: top and bottom -Left in an if statement in line 86 of BoardUIManager for use once side map movement is confirmed -Added MapSelection and SideMapControl panels into Unity scene for use in BoardUIManager -Button functions renamed/added for side view and top view; gave extra functionalities that now allow switching of map views and panels

--- Assets/Scripts/BoardUIManager.cs
+++ Assets/Scripts/BoardUIManager.cs
@@ -5,13 +5,20 @@ using UnityEngine.UI;
 
 public class BoardUIManager : MonoBehaviour
 {
-    public Camera boardCam;
-
+    //camera
+    public GameObject boardCam;
     public GameObject boardPlayerCamPoint;
-    public GameObject boardMapCamPoint;
+    public GameObject boardSideMapCamPoint;
+    public GameObject boardTopMapCamPoint;
+
+    //panels
     public GameObject mainPanel;
     public GameObject dicePanel;
     public GameObject itemsPanel;
+    public GameObject mapSelectionPanel;
+    public GameObject sidemapControlPanel;
+
+    public float speed;
 
     public void OpenDicePanel()
     {
@@ -20,6 +27,8 @@ public class BoardUIManager : MonoBehaviour
             mainPanel.SetActive(false);
             dicePanel.SetActive(true);
             itemsPanel.SetActive(false);
+            mapSelectionPanel.SetActive(false);
+            sidemapControlPanel.SetActive(false);
         }
     }
 
@@ -30,6 +39,8 @@ public class BoardUIManager : MonoBehaviour
             mainPanel.SetActive(false);
             dicePanel.SetActive(false);
             itemsPanel.SetActive(true);
+            mapSelectionPanel.SetActive(false);
+            sidemapControlPanel.SetActive(false);
         }
     }
 
@@ -40,16 +51,53 @@ public class BoardUIManager : MonoBehaviour
             mainPanel.SetActive(true);
             dicePanel.SetActive(false);
             itemsPanel.SetActive(false);
+            mapSelectionPanel.SetActive(false);
+            sidemapControlPanel.SetActive(false);
         }
     }
 
-    public void OpenBoardMapMode()
+    public void OpenMapSelectionPanel()
     {
-        
+        if (!mapSelectionPanel.activeInHierarchy)
+        {
+            mainPanel.SetActive(false);
+            dicePanel.SetActive(false);
+            itemsPanel.SetActive(false);
+            mapSelectionPanel.SetActive(true);
+            sidemapControlPanel.SetActive(false);
+        }
     }
 
     public void ReturnToPlayerCamMode()
     {
+        boardCam.transform.position = new Vector3(boardPlayerCamPoint.transform.position.x,
+            boardPlayerCamPoint.transform.position.y, boardPlayerCamPoint.transform.position.z);
+        boardCam.transform.eulerAngles = new Vector3(boardPlayerCamPoint.transform.eulerAngles.x,
+            boardPlayerCamPoint.transform.eulerAngles.y, boardPlayerCamPoint.transform.eulerAngles.z);
+    }
 
+    public void ChangeToSideCamMode()
+    {
+        boardCam.transform.position = new Vector3(boardSideMapCamPoint.transform.position.x,
+            boardSideMapCamPoint.transform.position.y, boardSideMapCamPoint.transform.position.z);
+        boardCam.transform.eulerAngles = new Vector3(boardSideMapCamPoint.transform.eulerAngles.x,
+            boardSideMapCamPoint.transform.eulerAngles.y, boardSideMapCamPoint.transform.eulerAngles.z);
+
+        if (!sidemapControlPanel.activeInHierarchy)
+        {
+            mainPanel.SetActive(false);
+            dicePanel.SetActive(false);
+            itemsPanel.SetActive(false);
+            mapSelectionPanel.SetActive(false);
+            sidemapControlPanel.SetActive(true);
+        }
+    }
+
+    public void ChangeToTopCamMode()
+    {
+        boardCam.transform.position = new Vector3(boardTopMapCamPoint.transform.position.x,
+            boardTopMapCamPoint.transform.position.y, boardTopMapCamPoint.transform.position.z);
+        boardCam.transform.eulerAngles = new Vector3(boardTopMapCamPoint.transform.eulerAngles.x,
+            boardTopMapCamPoint.transform.eulerAngles.y, boardTopMapCamPoint.transform.eulerAngles.z);
     }
 }
