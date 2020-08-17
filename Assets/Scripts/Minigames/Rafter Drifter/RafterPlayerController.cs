7122d4ccbf36446d3b19d0e2aa6dea220a23a164
Madshtik <jann_cosio@yahoo.com>
2020-08-18 01:27:31 +0400

    -Testing on Rafter Drifters: new movement controls - changed in movement script
    -Added 1 new folder called Players in the prefabs folder with 2 sub-folders for the Maxwll and Fiona Rivers models

--- Assets/Scripts/Minigames/Rafter Drifter/RafterPlayerController.cs
+++ Assets/Scripts/Minigames/Rafter Drifter/RafterPlayerController.cs
@@ -28,12 +28,16 @@ public class RafterPlayerController : MonoBehaviour
 
     [SerializeField]
     float jumpForce;
+    
+    [SerializeField]
+    float rotSpeed;
 
     int jumpCounter = 1;
 
     string jump;
     string movementH;
     string movementV;
+    string rotateH;
 
     // Start is called before the first frame update
     void Start()
@@ -50,7 +54,10 @@ public class RafterPlayerController : MonoBehaviour
         {
             Vector3 movement = new Vector3(Input.GetAxis(movementH), 0, Input.GetAxis(movementV));
 
-            rb.AddForce(movement * moveSpeed);
+            rb.AddForce(transform.forward * Input.GetAxis(movementV) * moveSpeed);
+            rb.AddForce(transform.right * Input.GetAxis(movementH) * moveSpeed);
+
+            transform.Rotate(0, Input.GetAxis(rotateH) * rotSpeed * Time.deltaTime, 0);
 
             Vector3 v = rb.velocity;
             v.x = Mathf.Clamp(v.x, 0, maxSpeed);
@@ -73,24 +80,28 @@ public class RafterPlayerController : MonoBehaviour
                 jump = "P1Fire1";
                 movementH = "P1Horizontal";
                 movementV = "P1Vertical";
+                rotateH = "P1RotateHorizontal";
                 break;
 
             case player.P2:
                 jump = "P2Fire1";
                 movementH = "P2Horizontal";
                 movementV = "P2Vertical";
+                rotateH = "P2RotateHorizontal";
                 break;
 
             case player.P3:
                 jump = "P3Fire1";
                 movementH = "P3Horizontal";
                 movementV = "P3Vertical";
+                rotateH = "P3RotateHorizontal";
                 break;
 
             case player.P4:
                 jump = "P4Fire1";
                 movementH = "P4Horizontal";
                 movementV = "P4Vertical";
+                rotateH = "P4RotateHorizontal";
                 break;
         }
     }
@@ -100,6 +111,12 @@ public class RafterPlayerController : MonoBehaviour
         if (collision.gameObject.CompareTag("Platform"))
         {
             jumpCounter = 1;
+        }
+
+        if (collision.gameObject.CompareTag("Finish"))
+        {
+            jumpForce = 0;
+            moveSpeed = 0;
         }
     }
 
