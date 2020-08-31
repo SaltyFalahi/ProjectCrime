3403db7829d531cafbeb2e5177c9f1c69b26ff92
Madshtik <jann_cosio@yahoo.com>
2020-08-31 05:56:47 +0400

    -Enabled the main menu and board UI to be controlled by the controller
    -Main menu now added to the build settings

---
+++ Assets/Scripts/MainMenuManager.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class MainMenuManager : MonoBehaviour
+{
+    [SerializeField]
+    GameObject firstMenuButton;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        EventSystem.current.SetSelectedGameObject(null);
+        EventSystem.current.SetSelectedGameObject(firstMenuButton);
+    }
+}
